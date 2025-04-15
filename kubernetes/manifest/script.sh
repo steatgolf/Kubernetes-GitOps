c40d007731960c50093dec5d1cda721752e65fe0
sarawoot.golf@hotmail.com <sarawoot.golf@hotmail.com>
2025-04-15 23:12:04 +0700

    edit argocd.yaml

--- kubernetes/manifest/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fail on error and undefined variables
-set -euo pipefail
-
-
-# Update and upgrade the system packages
-kubectl apply -f 1-namespace.yaml
-kubectl apply -f .
---
+++ kubernetes/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fail on error and undefined variables
+set -euo pipefail
+
+
+# Create namespace, deployment, service, ingress and ArgoCD application
+kubectl apply -f 1-namespace.yaml
+
+kubectl apply -f 2-deployment.yaml
+kubectl apply -f dev-deployment.yaml
+
+kubectl apply -f 3-service.yaml
+kubectl apply -f dev-service.yaml
+
+kubectl apply -f 4-ingress.yaml
+
+kubectl apply -f 5-argocd.yaml
